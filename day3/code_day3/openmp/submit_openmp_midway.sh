73637ba413066cdb7138629c46ea7d960af8dba3
Simon Scheidegger <simonsch@midway-login1.rcc.local>
2018-07-16 16:09:21 -0500

    updated submission script

--- day3/code_day3/openmp/submit_openmp_midway.sh
+++ day3/code_day3/openmp/submit_openmp_midway.sh
@@ -10,11 +10,11 @@
 #SBATCH --output=hello-openmp.out
 
 # this job requests node
-#SBATCH --ntasks=1
+#SBATCH --ntasks=8
 
 
 # and request 8 cpus per task for OpenMP threads
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 
 # this job will run in the sandyb partition on Midway1
 #SBATCH --partition=sandyb
@@ -26,4 +26,4 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 # Run the process with mpirun. Notice -n is not required. mpirun will
 # automatically figure out how many processes to run from the slurm options
 ### openmp executable
-./1.hello_world.exec
+./4.integration_pi.exec
