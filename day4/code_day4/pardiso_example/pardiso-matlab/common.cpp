8f5e89ad035a59412033d21fd8638c5a77cc9ea5
simon <simon.scheidegger@gmail.com>
2018-07-18 07:49:29 -0500

    updated lectures

--- day4/code_day4/pardiso_example/pardiso-matlab/common.cpp
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-// Function definitions.
-// -----------------------------------------------------------------
-bool mxIsDoubleScalar (const mxArray* ptr) {
-  return mxIsDouble(ptr) && mxGetNumberOfElements(ptr);
-}
--- day4/code_day4/pardiso_example/pardiso-matlab/readme.txt
+++
@@ -1,7 +0,0 @@
-* License details go here*
-
-For instructions and detailed help on how to install and use the
-MATLAB interface for PARDISO, please refer to the PARDISO Manual.
-
-Peter Carbonetto
-October 2009
